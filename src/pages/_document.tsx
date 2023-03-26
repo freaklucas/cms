bb68339b0026b2ac62570c627e49c1d7ecf2a14f
freaklucas <lucas.oliveira.freak@gmail.com>
2023-03-25 21:22:03 -0300

    feat:css padrão

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript  } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" 
+      />
+      <link rel="preconnect" href="https://fonts.gstatic.com" 
+      />
+      <link 
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;1,700;1,900&display=swap" 
+        rel="stylesheet"
+      />
+      <link rel="shortcut icon" href="/favicon.ico" type="image/png"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   )
-}
+}   
