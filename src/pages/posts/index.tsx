a12b456368d1858dba248d4cd7e2f295d7a6e5df
freaklucas <lucas.oliveira.freak@gmail.com>
2023-03-27 23:34:47 -0300

    feat:static ok

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "./style.module.scss";
 import Link from "next/link";
 import Image from "next/image";
-
+import { FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight} from 'react-icons/fi';
 import thumb from "../../../public/images/thumb.jpg";
 
 export default function Posts() {
@@ -27,6 +27,38 @@ export default function Posts() {
               <p>Hoje vou apresentar aspectos principais da lib React.</p>
             </a>
           </Link>
+          <div className={styles.buttonNavigate}>
+            <div>
+                <button>
+                    <FiChevronsLeft 
+                        size={25} 
+                        color="#fff" 
+                    />
+                </button>
+                <button>
+                    <FiChevronLeft 
+                        size={25} 
+                        color="#fff" 
+                    />
+                </button>
+            </div>
+
+            <div>
+                <button>
+                    <FiChevronsRight 
+                        size={25} 
+                        color="#fff" 
+                    />
+                </button>
+                <button>
+                    <FiChevronsRight 
+                        size={25} 
+                        color="#fff" 
+                    />
+                </button>
+            </div>
+
+          </div>
         </div>
       </main>
     </>
