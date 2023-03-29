f485139211897298f35949aca15fc8dffe5944cd
freaklucas <lucas.oliveira.freak@gmail.com>
2023-03-29 12:11:19 -0300

    feat:buscando datos e seguindo rota

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,13 +2,18 @@ import Head from "next/head";
 import styles from "./style.module.scss";
 import Link from "next/link";
 import Image from "next/image";
-import { FiChevronLeft, FiChevronsLeft, 
-  FiChevronRight, FiChevronsRight} from 'react-icons/fi';
-import thumb from "../../../public/images/thumb.jpg";
+import {
+  FiChevronLeft,
+  FiChevronsLeft,
+  FiChevronRight,
+  FiChevronsRight,
+} from "react-icons/fi";
+
 import { GetStaticProps } from "next";
-import {getPrismicClient} from '../../services/prismic';
+import { getPrismicClient } from "../../services/prismic";
 import Prismic from "@prismicio/client";
-import {RichText} from 'prismic-dom';
+import { RichText } from "prismic-dom";
+import { useState } from "react";
 
 type Post = {
   slug: string;
@@ -16,13 +21,14 @@ type Post = {
   cover: string;
   description: string;
   updatedAt: string;
-}
+};
 interface PostsProps {
   posts: Post[];
 }
 
-export default function Posts({posts}: PostsProps) {
-  console.log(posts);
+export default function Posts({ posts: postsBlog }: PostsProps) {
+  const [posts, setPosts] = useState(postsBlog || []);
+
   return (
     <>
       <Head>
@@ -30,51 +36,41 @@ export default function Posts({posts}: PostsProps) {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <Link href="/" as={`/`} passHref legacyBehavior>
-            <a>
-              <Image
-                src={thumb}
-                alt="banner"
-                width={720}
-                height={410}
-                quality={100}
-              />
-              <strong>Conheça o React</strong>
-              <time>Criado em 23 março de 2023.</time>
-              <p>Hoje vou apresentar aspectos principais da lib React.</p>
-            </a>
-          </Link>
+          {posts.map((post) => (
+            <Link key={post.slug} href={`posts/${post.slug}`} as={`posts/${post.slug}`} passHref legacyBehavior>
+              <a key={post.slug}>
+                <Image
+                  src={post.cover}
+                  alt={post.title}
+                  width={720}
+                  height={410}
+                  quality={100}
+                  placeholder="blur"
+                  blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkqPy/CQAEJQIssjE64AAAAABJRU5ErkJggg=="
+                />
+                <strong>{post.title}</strong>
+                <time>{post.updatedAt}</time>
+                <p>{post.description}</p>
+              </a>
+            </Link>
+          ))}
           <div className={styles.buttonNavigate}>
             <div>
-                <button>
-                    <FiChevronsLeft 
-                        size={25} 
-                        color="#fff" 
-                    />
-                </button>
-                <button>
-                    <FiChevronLeft 
-                        size={25} 
-                        color="#fff" 
-                    />
-                </button>
+              <button>
+                <FiChevronsLeft size={25} color="#fff" />
+              </button>
+              <button>
+                <FiChevronLeft size={25} color="#fff" />
+              </button>
             </div>
-
             <div>
-                <button>
-                    <FiChevronsRight 
-                        size={25} 
-                        color="#fff" 
-                    />
-                </button>
-                <button>
-                    <FiChevronsRight 
-                        size={25} 
-                        color="#fff" 
-                    />
-                </button>
+              <button>
+                <FiChevronsRight size={25} color="#fff" />
+              </button>
+              <button>
+                <FiChevronsRight size={25} color="#fff" />
+              </button>
             </div>
-
           </div>
         </div>
       </main>
@@ -82,32 +78,39 @@ export default function Posts({posts}: PostsProps) {
   );
 }
 
-export const getStaticProps : GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
-  const response = await prismic.query([
-    Prismic.predicates.at('document.type', 'post')
-  ], {
-    orderings: '[document.last_publication_date desc]',
-    fetch: ['post.title', 'post.description', 'post.cover'],
-    pageSize: 3,
-  });
-  const posts = response.results.map(post => {
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      orderings: "[document.last_publication_date desc]",
+      fetch: ["post.title", "post.description", "post.cover"],
+      pageSize: 3,
+    }
+  );
+  const posts = response.results.map((post) => {
     return {
       slug: post.uid,
       title: RichText.asText(post.data.title),
-      description: post.data.description.find((content: { type: string; }) => content.type === 'paragraph')?.text ?? '',
+      description:
+        post.data.description.find(
+          (content: { type: string }) => content.type === "paragraph"
+        )?.text ?? "",
       cover: post.data.cover.url,
-      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric'
-      })
-    }
-  })
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
   return {
     props: {
-      posts
+      posts,
     },
-    revalidate: 60*30
-  }
-}
+    revalidate: 60 * 30,
+  };
+};
